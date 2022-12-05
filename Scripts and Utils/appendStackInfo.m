c8318fae26fbec195d4cd8c96118e5e1417c773c
Michael Siniscalchi <michael.siniscalchi@princeton.edu>
2022-12-05 14:46:33 -0500

    Update appendStackInfo.m

--- Scripts and Utils/appendStackInfo.m
+++ Scripts and Utils/appendStackInfo.m
@@ -28,6 +28,6 @@ for i = 1:numel(data_dir)
         D = textscan(ImageDescription{1},'%s%s','Delimiter',{'='});
         stackInfo.startTime = str2num(D{2}{strcmp(D{1},'epoch ')});
         disp(['Editing stackInfo from session imaged on ' datestr(stackInfo.startTime,'yyyy-mm-dd') '...']);
-        %save(fullfile(batch_dir,data_dir(i),'stack_info.mat'),'-struct','-append');
+        save(fullfile(batch_dir,data_dir(i),'stack_info.mat'),'-struct','-append');
     end
 end
