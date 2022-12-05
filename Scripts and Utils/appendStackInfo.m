5937e45bb3f93e54eca075736e4db7962ebbc429
Michael Siniscalchi <michael.siniscalchi@princeton.edu>
2022-12-05 17:11:23 -0500

    Update appendStackInfo.m

--- Scripts and Utils/appendStackInfo.m
+++ Scripts and Utils/appendStackInfo.m
@@ -7,20 +7,20 @@ addGitRepo(dirs,'General','BrainCogs_mjs','iCorre-Registration');
 % List Data Dirs for Processing
 disp('Data Directories for Processing:')
 list = dir(fullfile(batch_dir, search_filter));
-list = list([list.isdir] & ~ismember({list.name},{'.','..'})); 
+list = list([list.isdir] & ~ismember({list.name},{'.','..'}));
 data_dir = string({list.name}');
 disp(data_dir);
 
-% Append new fields to stackInfo as needed 
+% Append new fields to stackInfo as needed
 for i = 1:numel(data_dir)
-    
+
     stackInfo_file = fullfile(batch_dir,data_dir(i),'stack_info.mat');
     if exist(stackInfo_file,'file')
         stackInfo = load(fullfile(batch_dir,data_dir(i),'stack_info.mat'));
     else
         continue
     end
-    
+
     raw_tiffs = dir(fullfile(batch_dir,data_dir(i),'raw','*.tif'));
     disp(['Getting session start-time from Tiff #1: ' raw_tiffs(1).name])
     if ~isfield(stackInfo,'startTime')
@@ -33,6 +33,8 @@ for i = 1:numel(data_dir)
             loadtiffseq(fullfile(batch_dir,data_dir(i),'raw',raw_tiffs(1).name),1); % load raw stack (.tif)
         D = textscan(ImageDescription{1},'%s%s','Delimiter',{'='});
         stackInfo.startTime = str2num(D{2}{strcmp(D{1},'epoch ')});
+        %Remove old field 'rawFileName'
+        stackInfo=rmfield(stackInfo,'rawFileName');
         %Save data structure
         disp(['Editing stackInfo from session imaged on ' datestr(stackInfo.startTime,'yyyy-mm-dd') '...']);
         save(fullfile(batch_dir,data_dir(i),'stack_info.mat'),'-struct','stackInfo','-append');
