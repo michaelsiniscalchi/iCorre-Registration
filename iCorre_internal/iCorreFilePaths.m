ec37353905d9eed6e24ec9486eef97c830294e2d
Michael Siniscalchi <michael.siniscalchi@princeton.edu>
2022-12-06 11:38:34 -0500

    Added paths for registered data to be used in braincogs_mjs repo

--- iCorre_internal/iCorreFilePaths.m
+++ iCorre_internal/iCorreFilePaths.m
@@ -5,6 +5,7 @@ dirs.main = fullfile(root_dir,data_dir);
 dirs.raw = fullfile(root_dir,data_dir,'raw'); %Raw data directory
 dirs.source = fullfile(root_dir,data_dir,source_dir); %Default is 'raw', but can be changed to allow for seed/rigid registration->cropping->registration
 dirs.mat = fullfile(root_dir,data_dir,'mat'); %temporary MAT file for pixel data ('stack') and transformations
+dirs.registered = fullfile(root_dir,data_dir,'registered-chan1'); %Registered data directory for channel-1
 
 %Path to Raw TIFFs
 tiffs = dir(fullfile(dirs.raw,'*.tif'));
@@ -19,10 +20,15 @@ paths.mat = cellfun(@(C) [C(1:end-4), '.mat'],...
 tiffs = dir(fullfile(dirs.source,'*.tif')); 
 paths.source = fullfile({dirs.source}',{tiffs(:).name}');
 
+%Registered Data from Channel 1, if existent
+tiffs = dir(fullfile(dirs.registered,'*.tif')); 
+paths.registered = fullfile({dirs.registered}',{tiffs(:).name}');
+
 %Metadata
 paths.regData = fullfile(root_dir,data_dir,'reg_info.mat'); %Matfile containing registration data
 paths.stackInfo = fullfile(root_dir,data_dir,'stack_info.mat'); %Matfile containing image header info and tag struct for writing to TIF
 
+% Convert all fullfile paths to string arrays
 fields = fieldnames(paths);
 for i = 1:numel(fields)
     paths.(fields{i}) = string(paths.(fields{i}));
