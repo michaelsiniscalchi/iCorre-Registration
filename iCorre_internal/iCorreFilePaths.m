cf7b75d2fb36b566a310ef00ae18f636a3e2ff5e
Michael Siniscalchi <michael.siniscalchi@princeton.edu>
2024-02-01 15:24:56 -0500

    Update iCorreFilePaths.m

--- iCorre_internal/iCorreFilePaths.m
+++ iCorre_internal/iCorreFilePaths.m
@@ -5,7 +5,18 @@ dirs.main = fullfile(root_dir,data_dir);
 dirs.raw = fullfile(root_dir,data_dir,'raw'); %Raw data directory
 dirs.source = fullfile(root_dir,data_dir,source_dir); %Default is 'raw', but can be changed to allow for seed/rigid registration->cropping->registration
 dirs.mat = fullfile(root_dir,data_dir,'mat'); %temporary MAT file for pixel data ('stack') and transformations
-dirs.registered = fullfile(root_dir,data_dir,'registered-chan1'); %Registered data directory for channel-1
+
+%Metadata
+paths.regData = fullfile(root_dir,data_dir,'reg_info.mat'); %Matfile containing registration data
+paths.stackInfo = fullfile(root_dir,data_dir,'stack_info.mat'); %Matfile containing image header info and tag struct for writing to TIF
+
+%Paths to Registered TIFFs
+dirs.registered = fullfile(root_dir,data_dir,'registered-chan0'); %Registered data directory for channel-0 (1-color imaging)
+
+s = load(paths.regData,'params'); %Check for 2-color imaging
+if s.params.reg_channel
+    dirs.registered = fullfile(root_dir,data_dir,['registered-chan', s.params.reg_channel]);
+end
 
 %Path to Raw TIFFs
 tiffs = dir(fullfile(dirs.raw,'*.tif'));
@@ -13,25 +24,16 @@ tiffs = dir(fullfile(dirs.raw,'*.tif'));
 paths.raw = fullfile({dirs.raw}',{tiffs(idx).name}');
 
 %Temporary MAT files for working memory
-paths.mat = cellfun(@(C) [C(1:end-4), '.mat'],... 
-    fullfile({dirs.mat}',{tiffs.name}'),'UniformOutput', false);
+paths.mat = string(cellfun(@(C) [C(1:end-4), '.mat'],... 
+    fullfile({dirs.mat}',{tiffs.name}'),'UniformOutput', false));
 
 %Source TIFFs for registration
 tiffs = dir(fullfile(dirs.source,'*.tif')); 
-paths.source = fullfile({dirs.source}',{tiffs(:).name}');
+paths.source = string(fullfile({dirs.source}',{tiffs(:).name}'));
 
 %Registered Data from Channel 1 and/or 2, if existent
-reg_dir = {...
-    fullfile(root_dir,data_dir,'registered-chan1'); 
-    fullfile(root_dir,data_dir,'registered-chan2')};
-for i = 1:2
-    tiffs = dir(fullfile(reg_dir{i},'*.tif'));
-    paths.registered{i} = string(fullfile(reg_dir(i)',{tiffs(:).name}'));
-end
-
-%Metadata
-paths.regData = fullfile(root_dir,data_dir,'reg_info.mat'); %Matfile containing registration data
-paths.stackInfo = fullfile(root_dir,data_dir,'stack_info.mat'); %Matfile containing image header info and tag struct for writing to TIF
+tiffs = dir(fullfile(dirs.registered,'*.tif'));
+paths.registered = string(fullfile({dirs.registered}',{tiffs(:).name}'));
 
 % Convert all fullfile paths to string arrays
 fields = fieldnames(paths);
